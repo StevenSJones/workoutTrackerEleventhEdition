f1136a9e0e5e049e1f184e07d79edbfa18a56405
SS Jones <SSJones.code@gmail.com>
2020-09-22 22:08:52 -0700

    I added the comments and the home route. The home route is the forward slash route and is located in the public dir and is called the index.html file.

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-//import the npm packages
+//import the npm packages needed
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require('body-parser');
+const path = require("path");
+
 //create express server
 const app = express();
 //set the port of the server allowing heroku to set
 const PORT = process.env.PORT || 8080;
 // Static directory - telling express where my statis files are
 app.use(express.static("public"));
+//api routes tells server to send index.html to browser
+app.get('/', function (req, res) {
+    res.send(path.resolve(__dirname, "public/index.html"))
+  })
 
+//set out port to listen
 app.listen(PORT, function() {
     console.log("server is listening on PORT: " + PORT);
 });
