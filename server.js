d41ce36481a54c6813188de06fd27f0360ea63d7
SS Jones <SSJones.code@gmail.com>
2020-09-22 21:51:46 -0700

    I included reference to static files in public dir.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+//import the npm packages
 const express = require("express");
 const morgan = require("morgan");
-
+const bodyParser = require('body-parser');
 //create express server
 const app = express();
 //set the port of the server allowing heroku to set
 const PORT = process.env.PORT || 8080;
+// Static directory - telling express where my statis files are
+app.use(express.static("public"));
 
 app.listen(PORT, function() {
     console.log("server is listening on PORT: " + PORT);
