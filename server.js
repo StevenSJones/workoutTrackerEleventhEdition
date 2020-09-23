3f704b5ab5a8e51c19484ade7fbfc92413a46c4f
SS Jones <SSJones.code@gmail.com>
2020-09-22 22:11:30 -0700

    I formatted the server.js file.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //import the npm packages needed
 const express = require("express");
 const morgan = require("morgan");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const path = require("path");
 
 //create express server
@@ -11,12 +11,10 @@ const PORT = process.env.PORT || 8080;
 // Static directory - telling express where my statis files are
 app.use(express.static("public"));
 //api routes tells server to send index.html to browser
-app.get('/', function (req, res) {
-    res.send(path.resolve(__dirname, "public/index.html"))
-  })
-
-//set out port to listen
-app.listen(PORT, function() {
-    console.log("server is listening on PORT: " + PORT);
+app.get("/", function (req, res) {
+  res.send(path.resolve(__dirname, "public/index.html"));
 });
-
+//set out port to listen
+app.listen(PORT, function () {
+  console.log("server is listening on PORT: " + PORT);
+});
