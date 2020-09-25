7b45ed6a6778495990015da1ec6d4fa8b54a80b8
SS Jones <SSJones.code@gmail.com>
2020-09-25 16:55:24 -0700

    I added comments in the server.js file to prep for group work and I wanted to push prior to the group work as well.

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ const mongoose = require("mongoose");
 const app = express();
 //set the port of the server allowing heroku
 const PORT = process.env.PORT || 8080;
-//Express middleware 
+//Express middleware
 // Static directory - telling express where my statis files are
 app.use(express.static("public"));
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(morgan("dev"));//logs out what we are going through
+app.use(morgan("dev")); //logs out what we are going through
 //mongoose
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useUnifiedTopology: true,//message came up in terminal to add this
+  useUnifiedTopology: true, //message came up in terminal to add this
 });
 // Mongodb error handling
 // db.on("error", error => {
@@ -32,19 +32,19 @@ app.get("/", function (req, res) {
 });
 //The /api/workouts route is defined
 app.get("/exercise", function (req, res) {
-  console.log("Inside the /api/workouts PUT route!!!!!!!!!!!!")
+  console.log("Inside the /api/workouts PUT route!!!!!!!!!!!!");
   res.sendFile(path.resolve(__dirname, "public/exercise.html"));
 });
 //The /stats route is defined
 app.get("/stats", function (req, res) {
-  console.log("Inside the /api/workouts PUT route!!!!!!!!!!!!")
+  console.log("Inside the /api/workouts PUT route!!!!!!!!!!!!");
   res.sendFile(path.resolve(__dirname, "public/exercise.html"));
 });
 //The /api/workouts route is defined
 app.get("/api/workouts", function (req, res) {
   res.send("Inside the /api/workouts route!!!!!!!!!!!!");
 });
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 //update (addExercise) dealing with information that we are passing along route. :id is called a route parameter. I am making a key of id and the value will be assigned.
 // app.put('/api/workouts/:id', function (req, res) {
 //   console.log("Inside the /api/workouts PUT route!!!!!!!!!!!!");
