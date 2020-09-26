2c519d6abb4a83e9f82b19e25b15805ff310dafd
SS Jones <SSJones.code@gmail.com>
2020-09-25 17:23:17 -0700

    I ammended the routes to all have a res. something so that they send a response and I stop getting errors regarding that.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const path = require("path");
 const mongoose = require("mongoose");
+//import models
+const model = require("./models");
 
 //create express server
 const app = express();
@@ -11,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 //Express middleware
 // Static directory - telling express where my statis files are
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("dev")); //logs out what we are going through
@@ -30,31 +32,32 @@ mongoose.connect("mongodb://localhost/workout", {
 app.get("/", function (req, res) {
   res.sendFile(path.resolve(__dirname, "public/index.html"));
 });
-//The /api/workouts route is defined
+//content route is defined
 app.get("/exercise", function (req, res) {
   console.log("Inside the /api/workouts PUT route!!!!!!!!!!!!");
   res.sendFile(path.resolve(__dirname, "public/exercise.html"));
 });
-//The /stats route is defined
+//The stats content route is defined
 app.get("/stats", function (req, res) {
   console.log("Inside the /api/workouts PUT route!!!!!!!!!!!!");
   res.sendFile(path.resolve(__dirname, "public/exercise.html"));
 });
+
+//api routes==============================================================
 //The /api/workouts route is defined
 app.get("/api/workouts", function (req, res) {
-  res.send("Inside the /api/workouts route!!!!!!!!!!!!");
+  res.send("Inside the /api/workouts get route!!!!!!!!!!!!");
 });
-app.use(express.static(path.join(__dirname, "public")));
-//update (addExercise) dealing with information that we are passing along route. :id is called a route parameter. I am making a key of id and the value will be assigned.
-// app.put('/api/workouts/:id', function (req, res) {
-//   console.log("Inside the /api/workouts PUT route!!!!!!!!!!!!");
-//   res.send(req.params)
-// });
-
-// app.post('/api/workouts', function (req, res) {
-//   console.log("Inside the /api/workouts POST route!!!!!!!!!!!!");
-// })
-
+// allows you to create a page using post
+app.post("/api/workouts", function (req, res) {
+  res.send("Inside the /api/workouts post route!!!!!!!!!!!!")
+  // console.log("Inside the /api/workouts POST route!!!!!!!!!!!!");
+});
+// update (addExercise) dealing with information that we are passing along route. :id is called a route parameter. I am making a key of id and the value will be assigned.
+app.put('/api/workouts/:id', function (req, res) {
+  // console.log("Inside the /api/workouts PUT route!!!!!!!!!!!!");
+  res.send(req.params)
+});
 //set out port to listen
 app.listen(PORT, function () {
   console.log("server is listening on PORT: " + PORT);
