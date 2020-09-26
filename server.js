b355428073300e94cc9cbd9867c889670dd29f84
SS Jones <SSJones.code@gmail.com>
2020-09-25 18:22:01 -0700

    dealt with the req in the api routes in the express server in the server.js file.

--- server.js
+++ server.js
@@ -46,17 +46,22 @@ app.get("/stats", function (req, res) {
 //api routes==============================================================
 //The /api/workouts route is defined
 app.get("/api/workouts", function (req, res) {
-  models.Workout.find().select("-_v").then(workouts => res.json(workouts));
+  models.Workout.find().select("-__v").then(workouts => res.json(workouts))
+  .catch(err => res.status(400).json(err));
   // res.send("Inside the /api/workouts get route!!!!!!!!!!!!");
 });
 // allows you to create a page using post
 app.post("/api/workouts", function (req, res) {
-  res.send("Inside the /api/workouts post route!!!!!!!!!!!!")
+  // res.send("Inside the /api/workouts post route!!!!!!!!!!!!")
+  models.Workout.create(req.body).then(workouts => res.json(workouts))
+  .catch(err => res.status(400).json(err));
   // console.log("Inside the /api/workouts POST route!!!!!!!!!!!!");
 });
 // update (addExercise) dealing with information that we are passing along route. :id is called a route parameter. I am making a key of id and the value will be assigned.
 app.put('/api/workouts/:id', function (req, res) {
   // console.log("Inside the /api/workouts PUT route!!!!!!!!!!!!");
+  models.Workout.findOneAndUpdate({_id:req.params.id}, req.body, {new:true}).then(workouts => res.json(workouts))
+  .catch(err => res.status(400).json(err));
   res.send(req.params)
 });
 //set out port to listen
